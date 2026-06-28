1ce07ab38e0e3821bd9bd7f6cf27a96eb9dcf03d
락현 <krh0323@pukyong.ac.kr>
2026-06-28 13:56:05 +0900

    [Chore] 백엔드 및 프론트엔드 프로젝트 세팅
    - 백엔드 프로젝트 생성 및 yml 설정 추가
    - 프론트엔드 프로젝트 생성 및 라이브러리 추가

---
+++ villo_backend/src/main/java/com/villo/VilloBackendApplication.java
@@ -0,0 +1,13 @@
+package com.villo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VilloBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VilloBackendApplication.class, args);
+    }
+
+}
