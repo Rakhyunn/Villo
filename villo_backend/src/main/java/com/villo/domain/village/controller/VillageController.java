dca5fd1de0dc7dc09a1cb4213816cfeb7efe1bda
락현 <krh0323@pukyong.ac.kr>
2026-07-02 20:36:47 +0900

    [Feat] 주민 데이터 세팅, 주민 상점 구현
    * [Feat] 주민 데이터 생성
    
    * [Feat] 주민 데이터 일부 수정
    
    - 겹치는 동물 개선
    - 기존 등급 개선
    
    * [Feat] 마을 주민 응답 dto 구현
    
    * [Feat] 주민 조회 및 영입 서비스 로직 구현
    
    - 주민 상점 조회 구현
    - 주민 영입 및 마을 레벨업 구현
    
    * [Feat] 주민 조회 및 영입 컨트롤러 구현

--- villo_backend/src/main/java/com/villo/domain/village/controller/VillageController.java
+++ villo_backend/src/main/java/com/villo/domain/village/controller/VillageController.java
@@ -1,7 +1,10 @@
 package com.villo.domain.village.controller;
 
 import com.villo.domain.village.dto.VillageNameUpdateRequest;
+import com.villo.domain.village.dto.VillagePeopleResponse;
 import com.villo.domain.village.dto.VillageResponse;
+import com.villo.domain.village.entity.type.VillagerGrade;
+import com.villo.domain.village.service.VillagePeopleService;
 import com.villo.domain.village.service.VillageService;
 import com.villo.global.response.ApiResponse;
 import jakarta.validation.Valid;
@@ -9,11 +12,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/village")
 @RequiredArgsConstructor
 public class VillageController {
     private final VillageService villageService;
+    private final VillagePeopleService villagePeopleService;
 
     // 내 마을 조회
     @GetMapping
@@ -30,5 +36,23 @@ public class VillageController {
             @Valid @RequestBody VillageNameUpdateRequest request
     ) {
         return ApiResponse.ok("마을 이름이 변경되었습니다.", villageService.updateVillageName(userId, request));
+    }
+
+    // 주민 목록 조회
+    @GetMapping("/people")
+    public ApiResponse<List<VillagePeopleResponse>> getVillagePeopleList(
+            @RequestParam(required = false) VillagerGrade grade
+    ) {
+        return ApiResponse.ok("주민 목록 조회 성공", villagePeopleService.getVillagePeopleList(grade));
+    }
+
+    // 주민 영입
+    @PostMapping("/people/{villagePeopleId}/buy")
+    public ApiResponse<VillagePeopleResponse> buyVillager(
+            @AuthenticationPrincipal Long userId,
+            @PathVariable Long villagePeopleId
+    ) {
+        return ApiResponse.ok("주민을 영입했습니다.",
+                villagePeopleService.buyVillager(userId, villagePeopleId));
     }
 }
