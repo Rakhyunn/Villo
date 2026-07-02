dedff634ce8d3c4c487a0fda76bd920a53ddec75
Rakhyunn <krh0323@pukyong.ac.kr>
2026-07-02 23:34:26 +0900

    [Feat] 주민 배치 레포지토리 구현

--- villo_backend/src/main/java/com/villo/domain/village/repository/VillagePlacementRepository.java
+++ villo_backend/src/main/java/com/villo/domain/village/repository/VillagePlacementRepository.java
@@ -4,8 +4,18 @@ import com.villo.domain.village.entity.VillagePlacement;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface VillagePlacementRepository extends JpaRepository<VillagePlacement, Long> {
     // 마을 배치 현황 조회
     List<VillagePlacement> findByUserVillageId(Long userVillageId);
+
+    // 유저의 주민인지 확인
+    boolean existsByUserVillagePeopleId(Long userVillagePeopleId);
+
+    // 유효한 범위의 좌표인지 확인
+    boolean existsByUserVillageIdAndGridXAndGridY(Long userVillageId, int gridX, int gridY);
+
+    // 주민이 있는 좌표인지 확인
+    Optional<VillagePlacement> findByUserVillageIdAndGridXAndGridY(Long userVillageId, int gridX, int gridY);
 }
